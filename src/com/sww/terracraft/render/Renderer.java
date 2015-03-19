3509b14dd373124571a71a7720710e6fe62a9fdd
Julien Kyou <yesimwearingpants@gmail.com>
2015-03-19 05:01:38 -0400

    Added License

--- src/com/sww/terracraft/render/Renderer.java
+++ src/com/sww/terracraft/render/Renderer.java
@@ -1,5 +1,9 @@
 package com.sww.terracraft.render;
 
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
@@ -9,6 +13,7 @@ import com.sww.terracraft.render.models.RawModel;
 public class Renderer {
 	
 	public static void prepare() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		GL11.glClearColor(0.09f, 0.05f, 0.1f, 1f);
 	}
 	
